1f78d58dde71abf1090997f4f4746c92b5444d96
Carol <carolhmj@gmail.com>
2015-06-19 19:11:20 -0300

    Função de iniciar leitura, inserir operação no conjunto de leitura/escrita e reiniciar transação

--- transação.py
+++ transação.py
@@ -3,11 +3,16 @@ from collections import namedtuple
 import re
 
 class Transação(object):
+	"""Estados possíveis em que uma transação pode estar"""
+	(INICIADA, LENDO, COMMITED) = (1, 2, 3)
 	"""docstring for Transação"""
 	def __init__(self, identificador, operações):
 		super(Transação, self).__init__()
 		self.identificador = identificador
 		self.operações = operações
+		self.estado = self.INICIADA
+		self.conjunto_leitura = set()
+		self.conjunto_escrita = set()
 
 	def primeiro_teste(Ti,Tj):
 		# Transaction Tj completa sua fase de escrita antes que Ti começou sua
@@ -38,6 +43,21 @@ class Transação(object):
 			operações = list(map(Operação._make,operações))
 			retorno.append(Transação(identificador,operações))
 		return retorno
+	def iniciar_leitura(timestamp):
+		self.estado = LENDO
+		self.timestamp_leitura = timestamp
+	def inserir_operação(operação):
+		"""Insere a operação no conjunto apropriado"""
+		if (operação.op == "w"):
+			self.conjunto_leitura.add(operação.objeto)
+		else:
+			self.conjunto_escrita.add(operação.objeto)
+	def reiniciar_transação(timestamp):
+		"""Reincia a transação"""
+		self.estado = self.INICIADA
+		self.conjunto_leitura.clear()
+		self.conjunto_escrita.clear()
+
 
 
 Operação = namedtuple("Operação",['op','objeto'])
