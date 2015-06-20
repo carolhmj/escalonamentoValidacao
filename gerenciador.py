1578a3b5830ed319933ec199e33798117517cfc7
Carol <carolhmj@gmail.com>
2015-06-19 22:41:32 -0300

    Classe de gerenciador de transação

---
+++ gerenciador.py
@@ -0,0 +1,72 @@
+from transação import Transação
+from transação import Operação
+import re
+from collections import namedtuple
+import os
+
+class Gerenciador(object):
+
+	def __init__(self, arquivo_transacoes):
+		super(Gerenciador, self).__init__()
+		self.lista_transacoes = Transação.ler_do_arquivo(arquivo_transacoes)
+
+	def ler_história(arquivo_história):
+		arquivo = open(arquivo_história,'r')
+		história = re.findall("(\S)(\d+?)\((\S+?)\)",arquivo.read())
+		história = list(map(OperaçãoGerenciador._make,história))
+		arquivo.close()
+		return história
+
+	def escrever_historia(história, arquivo):	
+		for operação in história:
+			arquivo.write(operação.op + operação.transação + "(" + operação.objeto + ")")
+		
+
+	def executar(self, arquivo_história, arquivo_saida):
+		história_inicial = Gerenciador.ler_história(arquivo_história)
+		#Inicialmente a história de saída é igual a história inicial, isso pode mudar quando retirarmos transações dela
+		história_saída = história_inicial
+
+		os.remove(arquivo_saida)
+		arquivo = open(arquivo_saida,'a+')
+		arquivo.write("Schedule de Entrada: ")
+		Gerenciador.escrever_historia(história_inicial, arquivo)
+
+		timestamp = 1
+		lista_commited = []
+		lista_cancelada = []
+		for operação in história_inicial:
+
+			transação = self.lista_transacoes[int(operação.transação)-1]
+			if (transação.estado == Transação.INICIADA):
+				transação.iniciar_leitura(timestamp)
+			if (transação.inserir_operação(Operação(operação.op, operação.objeto))):
+
+				if (transação.validar(timestamp,lista_commited)):
+					lista_commited.append(transação)
+				else:
+					transação.reiniciar_transação()
+					história_saída = filter(lambda x: x.transação != str(transação.identificador), história_saída)
+					lista_cancelada.append(transação)
+
+			timestamp = timestamp + 1
+		
+		arquivo.write("\nSchedule de Saída: ")
+		Gerenciador.escrever_historia(história_saída, arquivo)
+		arquivo.write("\nDeadlock: Não")
+		arquivo.write("\nTransações Abortadas: ")
+		i = 0
+		for l in lista_cancelada:
+			arquivo.write("T"+str(l.identificador))
+			i = i+1
+			if (i<len(lista_cancelada)):
+				arquivo.write(", ")
+		arquivo.write("\nTransações Efetivadas: ")
+		i = 0
+		for l in lista_commited:
+			arquivo.write("T"+str(l.identificador))
+			i = i+1
+			if (i<len(lista_commited)):
+				arquivo.write(", ")
+
+OperaçãoGerenciador = namedtuple("OperaçãoGerenciador",['op','transação','objeto'])		
--- transação.py
+++ transação.py
@@ -17,22 +17,36 @@ class Transação(object):
 	def primeiro_teste(Ti,Tj):
 		# Transaction Tj completa sua fase de escrita antes que Ti começou sua
 		# fase de leitura
-		return False
+		if (Tj.timestamp_commit < Ti.timestamp_leitura):
+			return True
+		else:
+			return False
 
 	def segundo_teste(Ti, Tj):
 		# Ti começa a sua fase de escrita depois que Tj completa sua fase de
 		# escrita e o conjunto de leitura de Ti não tem intersecção com o
 		# conjunto de escrita de Tj
-		return False
+		if (Ti.conjunto_leitura.isdisjoint(Tj.conjunto_leitura)):
+			return True
+		else:
+			return False
 
 	def terceiro_teste(Ti, Tj):
 		# A união do conjunto de leitura e escrita de Ti não tem intersecção
 		# com o conjunto de escrita de Tj e Tj completa sua fase de leitura antes de Ti.
-		return False
+		if (Ti.conjunto_leitura.isdisjoint(Tj.conjunto_escrita) and Ti.conjunto_escrita.isdisjoint(Tj.conjunto_escrita)):
+			return True
+		else:
+			return False
+		
 
-	def validar(Ti,comitadas):
+	def validar(Ti,timestamp,comitadas):
 		teste = lambda Tj: Ti.primeiro_teste(Tj) or Ti.segundo_teste(Tj) or Ti.terceiro_teste(Tj)
-		return all(map(teste,comitadas))
+		validada = all(map(teste,comitadas))
+		if (validada):
+			Ti.timestamp_commit = timestamp
+			Ti.estado = Ti.COMMITED
+		return validada
 
 	def ler_do_arquivo(caminho):
 		arquivo = open(caminho,"r");
@@ -43,16 +57,26 @@ class Transação(object):
 			operações = list(map(Operação._make,operações))
 			retorno.append(Transação(identificador,operações))
 		return retorno
-	def iniciar_leitura(timestamp):
-		self.estado = LENDO
+
+	def iniciar_leitura(self,timestamp):
+		self.estado = self.LENDO
 		self.timestamp_leitura = timestamp
-	def inserir_operação(operação):
-		"""Insere a operação no conjunto apropriado"""
+		self.numero_operações = 0
+
+	def inserir_operação(self,operação):
+		"""Insere a operação no conjunto apropriado. Retorna True se todas as operações daquela transação forem realizadas, assinalando a hora de dar commit"""
 		if (operação.op == "w"):
 			self.conjunto_leitura.add(operação.objeto)
 		else:
 			self.conjunto_escrita.add(operação.objeto)
-	def reiniciar_transação(timestamp):
+
+		self.numero_operações = self.numero_operações + 1
+		if (self.numero_operações == len(self.operações) ):
+			return True
+		else:
+			return False
+
+	def reiniciar_transação(self):
 		"""Reincia a transação"""
 		self.estado = self.INICIADA
 		self.conjunto_leitura.clear()
