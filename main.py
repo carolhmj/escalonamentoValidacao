b182657eb34cf278c331056707410ea64b774666
Daniel Augusto <danisson10@gmail.com>
2015-06-22 10:20:49 -0300

    Mudando a main

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ def main():
 
 	while not sair:
 
-		arquivo_história = input("Entre com o arquivo com a história das transações. Caso deseje inseri-las a \
-			partir do terminal, escreva \"stdin\": ")
-		arquivo_saída = input("Entre com o arquivo com a saída da execução da história. Caso deseje vê-la no \
-			terminal, escreva \"stdout\": ")
+		arquivo_história = input("Entre com o arquivo com a história das transações. Caso deseje inseri-las a"+
+		" partir do terminal, escreva \"stdin\": ")
+		arquivo_saída = input("Entre com o arquivo com a saída da execução da história. Caso deseje vê-la no"+
+		" terminal, escreva \"stdout\": ")
 
 		ger.executar(arquivo_história, arquivo_saída)
 
